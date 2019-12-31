3bd39252b38e9e75bff6dedd702953ae5967990e
lsh <zzlsh000@163.com>
2019-12-31 14:40:07 +0800

    git_test init

---
+++ src/main/java/com/lsh/git_test/GitTestApplication.java
@@ -0,0 +1,13 @@
+package com.lsh.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
